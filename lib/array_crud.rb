57dd8d7ba8910f6606ed3eb3736505416519c4fa
Nicole Lucien <nicolemlucien@gmail.com>
2019-09-23 00:26:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,7 +41,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  learning = ["wow", "I", "am", "really", "learning", "arrays!"]
+  learning[-1]
 end
 
 def update_element_from_index(array, index_number, element)
